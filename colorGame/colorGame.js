3215d261bd2bc0be0cdb2dbfd67b786cb484bbd4
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-07-08 18:19:42 -0700

    Generating updates.

--- colorGame/colorGame.js
+++ colorGame/colorGame.js
@@ -1,10 +1,12 @@
 var colors = generatedColors(6);
 var squares = document.querySelectorAll(".square");
-var head = document.querySelectorAll(".head");
-var pickedColor = pickedColor();
+var selectedColor = pickedColor();
 var rgb = document.getElementById("rgb");
-    rgb.textContent = pickedColor;
+    rgb.textContent = selectedColor;
 var message = document.getElementById("message");
+var newGame = document.getElementById("newGame");
+var headColor = document.getElementById("header");
+
 
 
 for (var i = 0; i < squares.length; i++) {
@@ -14,13 +16,14 @@ for (var i = 0; i < squares.length; i++) {
   //add click listeners to squares
   squares[i].addEventListener("click", function(){
     var clickedColor = this.style.backgroundColor;
-    if (clickedColor === pickedColor) {
+    if (clickedColor === selectedColor) {
       message.textContent = "That is correct!"
+      newGame.textContent = "Play Again?";
       changeColors(clickedColor);
+      headColor.style.background = clickedColor;
     }else{
       this.style.backgroundColor = "#544e4e";
       message.textContent = "DERP! No..."
-
     }
   });
 }
@@ -29,8 +32,7 @@ function changeColors(color){
   //loop through all the squares
   for (var i = 0; i < squares.length; i++) {
   //changes each color to match the given color
-    squares[i].style.backgroundColor = color
-    head[i].style.backgroundColor = color
+    squares[i].style.backgroundColor = color;
   }
 }
 
@@ -60,3 +62,20 @@ function randomColor(){
   var b = Math.floor(Math.random() * 256)
   return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+
+newGame.addEventListener("click", function(){
+  //generate new colors
+  colors = generatedColors(6);
+  //pick a new Colors
+  selectedColor = pickedColor();
+  // update header
+  rgb.textContent = selectedColor;
+  //change square Colors
+  for (var i = 0; i < squares.length; i++) {
+  //add initial colors to squares
+  squares[i].style.backgroundColor = colors[i];
+  //head color change
+  }
+  headColor.style.background = "#2b9dd6";
+  message = "";
+})
