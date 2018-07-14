5bc30c6389e17d08eac2d73b975faefe432de939
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-07-13 23:07:53 -0700

    addTodos

--- todo/assets/js/todo.js
+++ todo/assets/js/todo.js
@@ -1,22 +1,23 @@
 //Checks off completed Todos.
-$( "li" ).on("click", function(){
-  $( this ).toggleClass('completed');
+$( "ul" ).on( "click", "li", function(){
+  $( this ).toggleClass( 'completed' );
 });
 
 //delete //
-$( ".delete" ).on("click", function(event){
+$( "ul" ).on("click", "span", function(event){
   $( this ).parent().fadeOut(700, function(){
-    $(this).remove();
+    $( this ).remove();
   });
   event.stopPropagation();
 })
 
 //add todo
 $( ".addTodo" ).keypress(function(event){
-  if(event.which === 13){
+  if( event.which === 13 ){
     //grabs todo text from input
     var todoText = $( this ).val();
+    $( this ).val( "" );
     //create a new li and add to ul
-    $(".todoList").append("<li>todoText</li>")
+    $( ".todoList" ).append( "<li><span>x</span>" +  todoText + "</li>" )
   }
 });
